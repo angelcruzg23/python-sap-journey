17b554c4a8cc6d290cfa1ff4a92265c8f4146504
Angel Cruz <angelcruzg@gmail.com>
2026-01-10 17:24:30 -0500

    Creación Ayuda de Documentación

---
+++ Dia 5 /ComoUtilizarAyuda.py
@@ -0,0 +1,21 @@
+# dic = {
+#     'c1':100,
+#     'c2':200
+# }
+
+# print(dic)
+# remove = dic.popitem()
+# print(remove)
+# print(dic)
+
+dato = 'www.example.com'.lstrip('cmowz.')
+print(dato)
+
+marcas_smartphones = {"Samsung1", "Xiaomi", "Apple", "Huawei", "LG"}
+
+marcas_tv = {"Sony", "Philips", "Samsung", "LG"}
+
+conjuntos_aislados = marcas_smartphones.isdisjoint(marcas_tv)
+
+print(conjuntos_aislados)
+
